aa2ff691a2a5c3dc75d33bd3f822ae54aa0f6f05
whosNugget <19211255+not-nugget@users.noreply.github.com>
2022-02-10 16:03:40 -0500

    project restructure because of my OCD

---
+++ Uoiea/Models/AppConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Configuration;
+
+namespace Uoiea.Models
+{
+    public class AppConfig
+    {
+        public ILoggerFactory LoggerFactory { get; init; }
+        public string Token { get; init; }
+
+        internal AppConfig(ILoggerFactory loggerFactory)
+        {
+            LoggerFactory = loggerFactory;
+            Token = ConfigurationManager.AppSettings.Get("token");
+        }
+    }
+}
---
+++ Uoiea/Models/DiscordBot.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uoiea.Models
+{
+    public class DiscordBot
+    {
+        private DiscordClient Client { get; init; }
+
+        public DiscordBot(AppConfig config)
+        {
+            Client = new(new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.AllUnprivileged,
+                LoggerFactory = config.LoggerFactory,
+                Token = config.Token,
+            });
+        }
+    }
+}
