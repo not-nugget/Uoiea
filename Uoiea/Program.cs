e52f623094cd80ab05d7d9dad7fbc4eafa82fb00
not-nugget <19211255+not-nugget@users.noreply.github.com>
2022-11-10 19:16:50 -0600

    new cliwrap and program

--- Uoiea/Program.cs
+++ Uoiea/Program.cs
@@ -1,48 +1,77 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Threading.Tasks;
+using CliWrap;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
-using System;
-using System.Diagnostics;
-using System.Text;
-using System.Threading.Tasks;
-using Uoiea.Models;
 
 namespace Uoiea
 {
     static class Program
     {
+        private const string FFMPEG = "ffmpeg";
+        private const string FFMPEGPath = "ffmpegPath";
+        private static readonly string[] FFMPEGArguments = new[] { "" };
+
         static async Task Main(string[] args)
         {
             Logger logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-            ILoggerFactory factory = new LoggerFactory().AddSerilog(logger);
+            ILoggerFactory loggerFactory = new LoggerFactory().AddSerilog(logger);
 
-            using AppConfig config = new(factory);
-            using DiscordBot bot = new(config);
-
-            string handleA = config.TTSRead.GetClientHandleAsString(), handleB = config.TTSWrite.GetClientHandleAsString();
-
-            //start TTS speak process which connects to the pipe
-            ProcessStartInfo startInfo = new (@"E:\GitHub\Uoiea\SpeakServer\bin\Debug\SpeakServer.exe", string.Join(' ', handleA, handleB))
+            await
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((ctx, cfg) =>
             {
-                WindowStyle = ProcessWindowStyle.Normal,
-                UseShellExecute = false,
-                CreateNoWindow = false,
-            };
 
-            using Process ttsProcess = Process.Start(startInfo);
-
-            config.TTSRead.DisposeLocalCopyOfClientHandle();
-            config.TTSWrite.DisposeLocalCopyOfClientHandle();
-
-            await bot.StartAsync();
-
-            int input = 250;
-            byte[] read = new byte[input];
-            await config.TTSWrite.WriteAsync(Encoding.UTF8.GetBytes("test pipe write string"));
-            await config.TTSRead.ReadAsync(read.AsMemory(0, input));
-            Console.Write(Encoding.UTF8.GetString(read).ToCharArray(), 0, read.Length);
-
-            await Task.Delay(-1);
+            })
+            .ConfigureLogging((ctx, log) =>
+            {
+                log.ClearProviders();
+                log.AddSerilog(logger, true);
+            })
+            .ConfigureServices((ctx, svs) =>
+            {
+                svs.AddSingleton(loggerFactory);
+                svs.AddTransient(FfmpegCommandFactory);
+            })
+            .UseConsoleLifetime()
+            .Build()
+            .RunAsync();
         }
+
+        static Command FfmpegCommandFactory(IServiceProvider _) => Cli
+            .Wrap(FFMPEG)
+            .WithArguments(FFMPEGArguments)
+            .WithWorkingDirectory(FFMPEGPath);
+
+        //using AppConfig config = new(factory);
+        //using DiscordBot bot = new(config);
+
+        //string handleA = config.TTSRead.GetClientHandleAsString(), handleB = config.TTSWrite.GetClientHandleAsString();
+
+        ////start TTS speak process which connects to the pipe
+        //ProcessStartInfo startInfo = new (@"E:\GitHub\Uoiea\SpeakServer\bin\Debug\SpeakServer.exe", string.Join(' ', handleA, handleB))
+        //{
+        //    WindowStyle = ProcessWindowStyle.Normal,
+        //    UseShellExecute = false,
+        //    CreateNoWindow = false,
+        //};
+
+        //using Process ttsProcess = Process.Start(startInfo);
+
+        //config.TTSRead.DisposeLocalCopyOfClientHandle();
+        //config.TTSWrite.DisposeLocalCopyOfClientHandle();
+
+        //await bot.StartAsync();
+
+        //int input = 250;
+        //byte[] read = new byte[input];
+        //await config.TTSWrite.WriteAsync(Encoding.UTF8.GetBytes("test pipe write string"));
+        //await config.TTSRead.ReadAsync(read.AsMemory(0, input));
+        //Console.Write(Encoding.UTF8.GetString(read).ToCharArray(), 0, read.Length);
+
+        //await Task.Delay(-1);
     }
 }
